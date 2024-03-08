3d4ddf3477f7e81b3c88b0fbe1e87038dc462707
samleticias <sammyaleticia09@gmail.com>
2024-03-08 10:48:21 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import services.PrintService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        PrintService<Integer> ps = new PrintService<>();
+
+        System.out.print("Quantos valores vc quer digitar? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            Integer value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        Integer x = ps.first();
+        System.out.println("Primeiro: " + x);
+
+        sc.close();
+    }
+}
